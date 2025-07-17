151e741d66cf80f7d6cb318d464a46a778ca7540
gadharvey <joshe1590@gmail.com>
2025-07-17 18:22:29 -0500

    nine

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Webcam from "react-webcam";
 import "./App.css"; // si quieres más estilos luego
 
-const API_URL = "https://a36fa3105bd9.ngrok-free.app";
+const API_URL = "https://d12f880d746e.ngrok-free.app";
 
 function App() {
   const webcamRef = useRef(null);
